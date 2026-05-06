cde00c1726e4c7a1de278095972f3e6d17438608
leerlandais <leerlandais@gmail.com>
2026-05-06 17:54:46 +0200

    ...because I updated SQL

--- data/school_pages.sql
+++ data/school_pages.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 06, 2026 at 02:53 PM
+-- Generation Time: May 06, 2026 at 03:53 PM
 -- Server version: 8.4.7
 -- PHP Version: 8.4.15
 
@@ -30,11 +30,10 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `school_pages`;
 CREATE TABLE IF NOT EXISTS `school_pages` (
   `page_id` smallint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `page_slug` varchar(128) COLLATE utf8mb4_general_ci NOT NULL,
-  `page_tag_position` int UNSIGNED NOT NULL,
-  `page_tag_name` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
-  `page_tag_class` varchar(512) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `page_tag_content` varchar(2048) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `page_title` varchar(256) COLLATE utf8mb4_general_ci NOT NULL,
+  `page_slug` varchar(256) COLLATE utf8mb4_general_ci NOT NULL,
+  `page_created` datetime NOT NULL,
+  `page_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 COMMIT;
