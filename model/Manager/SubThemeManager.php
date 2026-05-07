741852a1871bc1eb436d8c287fed582a7ce0b338
leerlandais <leerlandais@gmail.com>
2026-05-07 20:10:19 +0200

    insert subTheme : OK

--- model/Manager/SubThemeManager.php
+++ model/Manager/SubThemeManager.php
@@ -6,5 +6,8 @@ use model\Abstract\AbstractManager;
 
 class SubThemeManager extends AbstractManager
 {
-
+    public function addSubTheme(array $data) : bool
+    {
+        return $this->insertAnything($data, "school_page_subcats");
+    }
 }
