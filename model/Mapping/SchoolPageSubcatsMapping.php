54baea6546f2e5ca88f07264573af082753271f1
leerlandais <leerlandais@gmail.com>
2026-05-08 19:04:31 +0200

    did the same for other mappings

--- model/Mapping/SchoolPageSubcatsMapping.php
+++ model/Mapping/SchoolPageSubcatsMapping.php
@@ -6,29 +6,50 @@ use model\Abstract\AbstractMapping;
 
 class SchoolPageSubcatsMapping extends AbstractMapping
 {
-    public int $subcat_id {
-        get => $this->_subcat_id;
-        set => $this->_subcat_id = $value;
+    public int $subcat_id;
+    public int $subcat_parent;
+    public string $subcat_name;
+    public bool $subcat_active;
+
+    public function getSubcatId(): int
+    {
+        return $this->subcat_id;
     }
 
-    public int $subcat_parent {
-        get => $this->_subcat_parent;
-        set => $this->_subcat_parent = $value;
+    public function setSubcatId(int $subcat_id): void
+    {
+        $this->subcat_id = $subcat_id;
     }
 
-    public string $subcat_name {
-        get => $this->_subcat_name;
-        set => $this->_subcat_name = $value;
+    public function getSubcatParent(): int
+    {
+        return $this->subcat_parent;
     }
 
-    public bool $subcat_active {
-        get => $this->_subcat_active;
-        set => $this->_subcat_active = $value;
+    public function setSubcatParent(int $subcat_parent): void
+    {
+        $this->subcat_parent = $subcat_parent;
     }
 
-    private int $_subcat_id;
-    private int $_subcat_parent;
-    private string $_subcat_name;
-    private bool $_subcat_active;
+    public function getSubcatName(): string
+    {
+        return $this->subcat_name;
+    }
+
+    public function setSubcatName(string $subcat_name): void
+    {
+        $this->subcat_name = $subcat_name;
+    }
+
+    public function isSubcatActive(): bool
+    {
+        return $this->subcat_active;
+    }
+
+    public function setSubcatActive(bool $subcat_active): void
+    {
+        $this->subcat_active = $subcat_active;
+    }
+
 
 }
--- model/Mapping/SchoolUsersMapping.php
+++ model/Mapping/SchoolUsersMapping.php
@@ -6,29 +6,19 @@ use model\Abstract\AbstractMapping;
 
 class SchoolUsersMapping extends AbstractMapping
 {
-    protected int $user_id {
-        get => $user_id;
-        set => $user_id = $value;
-    }
-
-    protected string $user_name {
-        get => $user_name;
-        set => $user_name = $value;
-    }
-
-    protected string $user_pass {
-        get => $user_pass;
-        set => $user_pass = $value;
-    }
-
-    protected string $user_role {
-        get => $user_role;
-        set => $user_role = $value;
-    }
+    protected int $user_id;
+    protected string $user_name;
+    protected string $user_pass;
+    protected string $user_role;
 
     public function getUserId(): int
     {
         return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): void
+    {
+        $this->user_id = $user_id;
     }
 
     public function getUserName(): string
@@ -36,15 +26,31 @@ class SchoolUsersMapping extends AbstractMapping
         return $this->user_name;
     }
 
+    public function setUserName(string $user_name): void
+    {
+        $this->user_name = $user_name;
+    }
+
     public function getUserPass(): string
     {
         return $this->user_pass;
+    }
+
+    public function setUserPass(string $user_pass): void
+    {
+        $this->user_pass = $user_pass;
     }
 
     public function getUserRole(): string
     {
         return $this->user_role;
     }
+
+    public function setUserRole(string $user_role): void
+    {
+        $this->user_role = $user_role;
+    }
+
 
 
 }
