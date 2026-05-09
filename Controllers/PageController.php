a757fe4ad93269fa2e75872cd2d5b50fd47fb736
leerlandais <leerlandais@gmail.com>
2026-05-09 12:08:55 +0200

    added new page form

--- Controllers/PageController.php
+++ Controllers/PageController.php
@@ -4,17 +4,23 @@ namespace Controllers;
 
 use Controllers\Abstract\AbstractController;
 use Factory\ManagerFactory;
+use model\Manager\MainThemeManager;
 use model\Manager\PageManager;
+use model\Manager\SubThemeManager;
 use Twig\Environment;
 
 class PageController extends Abstract\AbstractController
 {
     private PageManager $pageManager;
+    private SubThemeManager $subThemeManager;
+    private MainThemeManager $mainThemeManager;
 
     public function __construct(Environment $twig, ManagerFactory $managerFactory)
     {
         parent::__construct($twig, $managerFactory);
         $this->pageManager = $this->getManager(PageManager::class);
+        $this->subThemeManager = $this->getManager(SubThemeManager::class);
+        $this->mainThemeManager = $this->getManager(MainThemeManager::class);
     }
 
     public function addPage() : void
@@ -22,10 +28,18 @@ class PageController extends Abstract\AbstractController
         global $sessionRole, $systemMessage;
         $this->checkPermissions("ROLE_ADMIN", $sessionRole);
 
+        if(isset($_POST["unset:addNewPage"])) {
+            die(var_dump($_POST));
+        }
+
+        $mainThemes = $this->mainThemeManager->getThemes();
+        $subThemes = $this->subThemeManager->getSubThemes();
         echo $this->twig->render("private/page.add.html.twig", [
             "systemMessage" => $systemMessage,
             "sessionRole" => $sessionRole,
             "csrfToken" => $this->csrfToken,
+            "subThemes" => $subThemes,
+            "mainThemes" => $mainThemes,
         ]);
     }
 }
