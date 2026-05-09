196aaf481121c9e31e5778380836b9c960800583
leerlandais <leerlandais@gmail.com>
2026-05-09 07:00:56 +0200

    fixed theme output

---
+++ Controllers/PageController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Controllers;
+
+use Controllers\Abstract\AbstractController;
+
+class PageController extends Abstract\AbstractController
+{
+
+}
---
+++ model/Manager/PageManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace model\Manager;
+
+use model\Abstract\AbstractManager;
+
+class PageManager extends AbstractManager
+{
+
+}
