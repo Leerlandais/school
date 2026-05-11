1bba91e50343e3e31d2f75d226d3aaed8ac71d66
leerlandais <leerlandais@gmail.com>
2026-05-11 10:29:16 +0200

    sql update

---
+++ data/school_tags.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 11 mai 2026 à 08:28
+-- Version du serveur : 8.4.7
+-- Version de PHP : 8.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `school_tags`
+--
+
+DROP TABLE IF EXISTS `school_tags`;
+CREATE TABLE IF NOT EXISTS `school_tags` (
+  `tag_id` tinyint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `tag_has_close` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
