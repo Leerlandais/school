f7c8a0674c8eecf77b84eadd1193ba167cd8f41c
leerlandais <leerlandais@gmail.com>
2026-05-17 18:08:20 +0200

    removed br usage

--- public/scripts/deleteConfirmation.js
+++ public/scripts/deleteConfirmation.js
@@ -1,7 +1,9 @@
-const delButton = document.getElementById("delButton")
-delButton.addEventListener("click", () => {
-    const confirmation = confirm("Are you sure you want to delete this?")
-    if (confirmation) {
-        document.getElementById("deleteForm").submit()
-    }
-});
+if(document.getElementById("delButton") != null) {
+    const delButton = document.getElementById("delButton")
+    delButton.addEventListener("click", () => {
+        const confirmation = confirm("Are you sure you want to delete this?")
+        if (confirmation) {
+            document.getElementById("deleteForm").submit()
+        }
+    });
+}
